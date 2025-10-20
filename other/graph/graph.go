c13f8ae2220259efb612754cca89ebbadb35458c
hgolebi <hugo7744@gmail.com>
2025-10-20 20:42:00 +0200

    saving network in JSON and DOT formats

---
+++ other/graph/graph.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/yaricom/goNEAT/v4/neat/genetics"
+	"github.com/yaricom/goNEAT/v4/neat/network/formats"
+)
+
+func main() {
+	// Retrieve the genome file path from command line arguments
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: go run graph.go <genome_file_path>")
+	}
+	filePath := os.Args[1]
+	useDotFormat := false
+	if len(os.Args) > 2 && os.Args[2] == "--dot" {
+		useDotFormat = true
+	}
+	genomeReader, err := genetics.NewGenomeReaderFromFile(filePath)
+	if err != nil {
+		log.Fatal("Failed to create genome reader:", err)
+	}
+	startGenome, err := genomeReader.Read()
+	if err != nil {
+		log.Fatal("Failed to read genome:", err)
+	}
+	net, err := startGenome.Genesis(1)
+	if err != nil {
+		log.Fatal("Failed to create network from genome:", err)
+	}
+
+	if useDotFormat {
+		dotFile, err := os.OpenFile("graph.dot", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+		if err != nil {
+			log.Fatal("Failed to open dot file:", err)
+		}
+		defer dotFile.Close()
+		err = formats.WriteDOT(dotFile, net)
+		if err != nil {
+			log.Fatal("Failed to write file:", err)
+		}
+	} else {
+		jsonFile, err := os.OpenFile("graph.json", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+		if err != nil {
+			log.Fatal("Failed to open json file:", err)
+		}
+		defer jsonFile.Close()
+		err = formats.WriteCytoscapeJSON(jsonFile, net)
+		if err != nil {
+			log.Fatal("Failed to write file:", err)
+		}
+	}
+}
